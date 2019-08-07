31e0fd7222bdd7f44eb67c38979f5dacd92da2ed
Nguyễn Lê Thành Công <33917105+nguyenlethanhcong@users.noreply.github.com>
2019-08-07 11:21:02 +0700

    Code write with CodeIgniter framework

---
+++ application/views/admin/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+		<div class="wrapper">
+		Bản quyền &copy; 2018-2019
+		<p>kết bạn: <a href="https://www.facebook.com/congpro.yeudonphuong">https://www.facebook.com/congpro.yeudonphuong</a></p>
+		 
+		</div>
+		
+</div>
